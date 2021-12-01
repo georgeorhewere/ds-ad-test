9fa33f106a55de9ce6fe300074b51e91839c0816
George Iruobe Orhewere <george.orhewere@quomodosystems.com>
2021-12-01 11:19:55 +0100

    refactor: use environment file for app credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -10,7 +10,7 @@ export const environment = {
   tenantId: 'd75facc4-6467-47d9-ac73-f8fcb4ba0845',  
   clienId: '6d045903-d5fe-40ad-82b5-c74b9dab40e6',
   domainUrl:'docstreamdev.onmicrosoft.com',  
-  redirectUrl: 'http://localhost:4200'  
+  authRedirectUrl: '/validate-user'  
 };
 
 /*
